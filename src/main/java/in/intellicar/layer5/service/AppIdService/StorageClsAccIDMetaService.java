4140a0b9800df74bcd8a76f1bfcb45412701860b
evmohanSmartcar <ev.mohan@intellicar.in>
2021-03-02 12:36:16 +0530

    Initial commit

---
+++ src/main/java/in/intellicar/layer5/service/AppIdService/StorageClsAccIDMetaService.java
@@ -0,0 +1,12 @@
+package in.intellicar.layer5.service.AppIdService;
+
+/**
+ * @author krishna mohan
+ * @version 1.0
+ * @project StorageClsAccIDMetaService
+ * @date 01/03/21 - 4:38 PM
+ */
+public class StorageClsAccIDMetaService
+{
+
+}
